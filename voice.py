d75614ea6f26c48c1cc80234754f841fb8e729ed
Maciej <maciej@Maciej>
2015-06-13 13:49:34 +0200

    voice.py, return

--- voice.py
+++ voice.py
@@ -4,9 +4,9 @@ import string
 
 class AIController:
 
-	def __init__(self, previous_orders, anger_meter):
-		self.previous_orders = previous_orders
-		self.anger_meter = anger_meter
+	def __init__(self):
+		self.previous_orders = 0
+		self.anger_meter = 0
 
 
 	def act(self, order):
@@ -34,34 +34,34 @@ class AIController:
 		if "show_console" in order:
 			self.show_console()
 
-                return(rep)
+		return(rep)
 
 	def track(self, order):
 		before = order.find('track')
 		if "do not" in order[:before]:
 			if self.previous_orders == 0:
-				self.call ('"But I am not tracking you, master"')
+				return ('"But I am not tracking you, master"')
 				if self.anger_meter != 10:
 					self.anger_meter += 1
 			else:
 				if self.anger_meter == 10:
-					self.call ('"NO!"')
+					return ('"NO!"')
 				else :
-					self.call ('"As you wish sire, I will stop tracking your face"')
+					return ('"As you wish sire, I will stop tracking your face"')
 					#here we stop tracking
 				self.previous_orders = 0
 				if self.anger_meter != 0:
 					self.anger_meter -= 1
 		else :
 			if self.previous_orders == 1:
-				self.call('"I am already tracking you"')
+				return('"I am already tracking you"')
 				if self.anger_meter != 10:
 					self.anger_meter += 1
 			else:
 				if self.anger_meter == 10:
-					self.call ('"NO!"')
+					return ('"NO!"')
 				else :
-					self.call('"Yes master, I will track you"')
+					return('"Yes master, I will track you"')
 					#we begin tracking
 				self.previous_orders = 1
 				if self.anger_meter != 0:
@@ -75,22 +75,20 @@ class AIController:
 		city_id = self.get_city_id(loc_id)
 		all_info = pywapi.get_weather_from_weather_com( city_id , units = 'metric' )
 		weather = all_info['current_conditions']['temperature'] 
-   		self.call('Temperature:')
-		self.call(weather)
-		self.call('"celsius degrees in"')
-		self.call(city)
+		weather_respone = ('Temperature:' + weather + '"celsius degrees in"' + city)
+   		return (weather_respone)
 
 	def turn_off(self):
-		self.call('"turning off"')
+		return('"turning off"')
     	#turn off
 
 	def show_console(self):
     	#we show console here
-		self.call('"I will now show you console"')
+		return('"I will now show you console"')
 
 	def hide_console(self):
     	#we hide console
-		self.call('"I will now hide console"')
+		return('"I will now hide console"')
 
 	def relation(self, order):
 		end = order.find('love')
@@ -110,7 +108,7 @@ class AIController:
 			return key
 
 	def greet(self, order):
-		self.call ('"Hello!"')
+		return ('"Hello!"')
 
 class VoiceResponse:
 	def call(self, text):
