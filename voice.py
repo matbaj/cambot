bbcff0d724d9d0e78e3c2568eff3d190119718da
matbaj <9reyfox@gmail.com>
2015-06-13 15:25:06 +0200

    removed sound samples and add simple implementation to voice.py

--- voice.py
+++ voice.py
@@ -1,8 +1,25 @@
 import subprocess
+import speech_recognition as sr
 
 class VoiceResponse:
 	def say(self, text):
 		subprocess.call("espeak \"%s\"" %(text), shell=True)
 
+class VoiceRecogniser:
+    def __init__(lang="pl"):
+        self.recorder = sr.Recognizer(language=lang)
+        self.thread = None
 
+    def start(self):
+        self.thread = self.recorder.listen_in_background(sr.Microphone(), self.callback)
+
+    def stop(self):
+        
+
+    def callback(self,recognizer, audio):                          # this is called from the background thread
+        try:
+            return(recognizer.recognize(audio))  # received audio data, now need to recognize it
+        except LookupError:
+            print "Recognision Error" 
+            return(-1)
 
